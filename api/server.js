2de83412cc6b9ecb7cbe9b2675327d0029c3b136
Bouratchik <jbouratchik@gmail.com>
2021-12-13 21:37:26 +0100

    # structure revamp + client

---
+++ api/server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = require('./app')
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+app.use(express.json())
+
+let server
+mongoose.connect(`mongodb://127.0.0.1:27017/${process.env.DB_NAME}`, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
+	console.log('db ok')
+	server = app.listen(process.env.APP_PORT, () => {
+		console.log(`Listening to port ${process.env.APP_PORT}`)
+	})
+})
+
+process.on('SIGTERM', () => {
+	logger.info('SIGTERM ok')
+	if (server) {
+		server.close()
+	}
+})
